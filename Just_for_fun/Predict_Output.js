d860b4d754bed32cb2ae224d4e954944622003b3
kumar-atul-au7 <atultunnu2@gmail.com>
2020-04-11 22:15:19 +0530

    Update Predict_Output.js

--- Just_for_fun/Predict_Output.js
+++ Just_for_fun/Predict_Output.js
@@ -1,24 +1,23 @@
-const limit=3;
 function callViaTimeOut(n){
-    if(n>limit){
+    if(n<=0){
         return
     }
-    setTimeout(callViaTimeOut,0,n+1); //setTimeout(function_to_be_called,after time,parameters_of_function)
+    setTimeout(callViaTimeOut,0,n-1); //setTimeout(function_to_be_called,after time,parameters_of_function)
     console.log(n)
 }
 
 function callDirectly(n){
-    if(n>limit){
+    if(n<=0){
         return
     }
-    callDirectly(n+1);
+    callDirectly(n-1);
     console.log(n)
 }
 
 
 //console.log("Calling callViaTimeOut(1)");
-callViaTimeOut(1);
+callViaTimeOut(3);
 //console.log("Execution returned from callViaTimeOut");
 //console.log("Calling callDirectly(1)");
-callDirectly(1);
+callDirectly(3);
 //console.log("Execution returned from callDirectly. No further line of code to execute/Not really.");
