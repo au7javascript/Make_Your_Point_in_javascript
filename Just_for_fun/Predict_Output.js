145dc80252d9425295c7544725498f8d91fc307d
Kumar Atul <atultunnu2@gmail.com>
2020-04-11 22:06:24 +0530

    Recursion and setTimeout

---
+++ Just_for_fun/Predict_Output.js
@@ -0,0 +1,24 @@
+const limit=3;
+function callViaTimeOut(n){
+    if(n>limit){
+        return
+    }
+    setTimeout(callViaTimeOut,0,n+1);
+    console.log(n)
+}
+
+function callDirectly(n){
+    if(n>limit){
+        return
+    }
+    callDirectly(n+1);
+    console.log(n)
+}
+
+
+//console.log("Calling callViaTimeOut(1)");
+callViaTimeOut(1);
+//console.log("Execution returned from callViaTimeOut");
+//console.log("Calling callDirectly(1)");
+callDirectly(1);
+//console.log("Execution returned from callDirectly. No further line of code to execute/Not really.");
