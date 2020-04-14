a0c0c960c2f145397a1d2341512f364e82c61393
Anik Ghosh <tech.anikghosh@gmail.com>
2020-04-14 17:11:14 +0530

    quick sort algorithm

--- Sorting_algo/quick_sort.js
+++ Sorting_algo/quick_sort.js
@@ -1,4 +1,22 @@
-let arr1 = [3, 8, 7, 6, -4,  2, 1, 5];
+// input
+// [4, 6, 9, 8, 2, 3, 7]
+
+// output
+// [2, 3, 4, 6, 7, 8, 9]
+
+// explanation
+// [4, 6, 9, 8, 2, 3, 7]     input
+// [2, 3] - [4] - [6, 9, 8, 7]       stage 1
+// [] = [2] = [3] - [4] - [] = [6] = [9, 8, 7]       stage 2
+// [] = [2] = [3] - [4] - [] = [6] = [8, 7] . [9] . []        stage 3
+// [] = [2] = [3] - [4] - [] = [6] = [7] # [8] # [] . [9] . []       stage 4
+// [] = [2] = [3] - [4] - [] = [6] = [7, 8] . [9] . []       concatenation result of stage 4 for stage 3
+// [] = [2] = [3] - [4] - [] = [6] = [7, 8, 9]       concatenation result of stage 3 for stage 2
+// [2, 3] - [4] - [6, 7, 8, 9]       concatenation result of stage 2 for stage 1
+// [2, 3, 4, 6, 7, 8, 9]     output
+
+
+let arr1 = [4, 6, 9, 8, 2, 3, 7];
 function quickSort(arr) {
     if (arr.length <= 1) {
         return arr;
